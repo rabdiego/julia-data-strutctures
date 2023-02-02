82f8030ad51571bfe40d668ab3e1a34cdc296bea
nikrs14 <diegorabelodesa@gmail.com>
2023-02-02 16:48:54 -0300

    Some features implemented in stack.jl

---
+++ stack.jl
@@ -0,0 +1,48 @@
+mutable struct Node
+    key::Union{Int64, Float64}
+    next::Union{Node, Nothing}
+end
+
+mutable struct Stack
+    top::Union{Node, Nothing}
+end
+
+function newStack()::Stack
+    return Stack(nothing)
+end
+
+function printStack(stack::Union{Stack, Nothing})
+    if isnothing(stack.top)
+        println("[]")
+    else
+        aux = stack.top
+        print("[$(aux.key)")
+        while !isnothing(aux.next)
+            aux = aux.next
+            print(", $(aux.key)")
+        end
+        println("]")
+    end
+end
+
+function push(stack::Union{Stack, Nothing}, key::Union{Int64, Float64})::Stack
+    aux = Node(key, stack.top)
+    stack.top = aux
+    return stack
+end
+
+function pop(stack::Union{Stack, Nothing})::Stack
+    if isnothing(stack.top)
+        println("Error: Underflow")
+    else
+        stack.top = stack.top.next
+    end
+    return stack
+end
+
+s = newStack()
+s = push(s, 4)
+s = push(s, 5.0)
+s = push(s, 7.3)
+s = pop(s)
+printStack(s)
